b7af75d01c4d4712196e94b0b3f8c63e4c9cc4f8
Akash Saha <shadowakashsaha@gmail.com>
2025-04-01 09:08:24 +0600

    complete the color effect

--- script.js
+++ script.js
@@ -72,10 +72,13 @@ function showQuestion() {
     button.innerHTML = answer.text;
     button.classList.add("btn");
     answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+
+    button.addEventListener("click", selectAnswer);
   });
 }
-
-startQuiz();
 
 function resetState() {
   nextButton.style.display = "none";
@@ -83,3 +86,15 @@ function resetState() {
     answerButtons.removeChild(answerButtons.firstChild);
   }
 }
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+}
+
+startQuiz();
